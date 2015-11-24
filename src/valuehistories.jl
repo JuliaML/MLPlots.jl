ba09fb959fe5621355abe06c8e2850091251a4ce
Christof Stocker <christof.stocker@aimc.at>
2015-11-24 19:25:17 +0100

    use _apply_recipe for valuehistories

--- src/valuehistories.jl
+++ src/valuehistories.jl
@@ -14,39 +14,36 @@ function _filter_plotable_histories(h::DynMultivalueHistory)
     filtered
 end
 
-function Plots.plot{I,V<:Real}(h::VectorUnivalueHistory{I,V}; marker = (:ellipse, 1.5), leg = false, kw...)
-    plot(get(h)...; marker = marker, leg = leg, kw...)
+function Plots._apply_recipe{I,V<:Real}(d::Dict, h::VectorUnivalueHistory{I,V}; kw...)
+    d[:markershape] = get(d, :markershape, :ellipse)
+    d[:markersize] = get(d, :markersize, 1.5)
+    d[:legend] = get(d, :legend, false)
+    get(h)
 end
 
-function Plots.plot!{I,V<:Real}(plt::Plots.Plot, h::VectorUnivalueHistory{I,V}; marker = (:ellipse, 1.5), kw...)
-    plot!(plt, get(h)...; marker = marker, kw...)
+function Plots._apply_recipe{I,V<:Real}(d::Dict, h::QueueUnivalueHistory{I,V}; kw...)
+    d[:markershape] = get(d, :markershape, :ellipse)
+    d[:markersize] = get(d, :markersize, 1.5)
+    d[:legend] = get(d, :legend, false)
+    get(h)
 end
 
-function Plots.plot{I,V<:Real}(h::QueueUnivalueHistory{I,V}; marker = (:ellipse, 1.5), leg = false, kw...)
-    plot(get(h)...; marker = marker, leg = leg, kw...)
-end
-
-function Plots.plot!{I,V<:Real}(plt::Plots.Plot, h::QueueUnivalueHistory{I,V}; marker = (:ellipse, 1.5), kw...)
-    plot!(plt, get(h)...; marker = marker, kw...)
-end
-
-function Plots.plot(h::DynMultivalueHistory; title = "Multivalue History", leg = true, kw...)
-    h_vec = [(k, v) for (k, v) in _filter_plotable_histories(h)]
-    if length(h_vec) > 0
-        plt = plot(h_vec[1][2]; label = string(h_vec[1][1]), title = title, leg = leg, kw...)
-        for i = 2:length(h_vec)
-            plot!(plt, h_vec[i][2]; label = string(h_vec[i][1]))
+function Plots._apply_recipe(d::Dict, h::DynMultivalueHistory; kw...)
+    filtered = _filter_plotable_histories(h)
+    k_vec = [k for (k, v) in filtered]
+    v_vec = [v for (k, v) in filtered]
+    if length(v_vec) > 0
+        d[:markershape] = get(d, :markershape, fill(:ellipse, 1, length(v_vec)))
+        d[:markersize] = get(d, :markersize, fill(1.5, 1, length(v_vec)))
+        d[:title]  = get(d, :title, "Multivalue History")
+        d[:legend] = get(d, :legend, true)
+        d[:label]  = get(d, :label, map(string, k_vec)')
+        if get(d, :n, 0) == length(v_vec)
+            d[:legend] = false
+            d[:title] = d[:label]
         end
-        plt
-    else
-        throw(ArgumentError("Can't plot an empty history, nor a history with strange types"))
-    end
-end
-
-function Plots.subplot(h::DynMultivalueHistory; linkx = true, kw...)
-    plts = [plot(v; title = string(k)) for (k, v) in _filter_plotable_histories(h)]
-    if length(plts) > 0
-        subplot(plts...; linkx = linkx, kw...)
+        get_vec = map(get, v_vec)
+        [x for (x, y) in get_vec], [y for (x, y) in get_vec]
     else
         throw(ArgumentError("Can't plot an empty history, nor a history with strange types"))
     end
