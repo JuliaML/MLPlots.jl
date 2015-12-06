558eca032dd28838b3325f64bec2b47c7e3f3040
Christof Stocker <christof.stocker@aimc.at>
2015-12-06 22:01:36 +0100

    use _apply_recipe for losses

--- src/LearnBase/loss.jl
+++ src/LearnBase/loss.jl
@@ -6,48 +6,28 @@ typealias DLoss DistanceBasedLoss
 _loss_xlabel(loss::CLoss) = "y ⋅ h(x)"
 _loss_xlabel(loss::DLoss) = "h(x) - y"
 
-function Plots.plot(loss::SupervisedLoss; kw...)
-    Plots.plot(loss, -2, 2; kw...)
+function Plots._apply_recipe(d::Dict, loss::SupervisedLoss; kw...)
+    Plots._apply_recipe(d, loss, -2, 2; kw...)
 end
 
-function Plots.plot(loss::SupervisedLoss, args...;
-                    xlabel = _loss_xlabel(loss),
-                    ylabel = "L(y, h(x))",
-                    label = string(loss),
-                    kw...)
-    plot(value_fun(loss), args...;
-         xlabel = xlabel,
-         ylabel = ylabel,
-         label = label,
-         kw...)
+function Plots._apply_recipe(d::Dict, loss::SupervisedLoss, args...; kw...)
+    get!(d, :xlabel, _loss_xlabel(loss))
+    get!(d, :ylabel, "L(y, h(x))")
+    get!(d, :label, string(loss))
+    value_fun(loss), args...
 end
 
-function Plots.plot{T<:SupervisedLoss}(vec::Vector{T}; kw...)
-    Plots.plot(vec, -2, 2; kw...)
+function Plots._apply_recipe{T<:SupervisedLoss}(d::Dict, losses::AbstractVector{T}; kw...)
+    Plots._apply_recipe(d, losses, -2, 2; kw...)
 end
 
-function Plots.plot{T<:SupervisedLoss}(vec::Vector{T}, args...; kw...)
-    @assert length(vec) > 0
-    plt = plot(vec[1], args...; kw...)
-    for loss in vec[2:end]
-        plot!(plt, loss, args...)
+function Plots._apply_recipe{T<:SupervisedLoss}(d::Dict, losses::AbstractVector{T}, args...; issubplot = false, kw...)
+    get!(d, :xlabel, _loss_xlabel(first(losses)))
+    get!(d, :ylabel, "L(y, h(x))")
+    get!(d, :label, map(string, losses)')
+    if issubplot && (get!(d, :n, length(losses)) == length(losses))
+        d[:legend] = false
+        d[:title] = d[:label]
     end
-    plt
-end
-
-function Plots.plot!(plt::Plots.Plot, loss::SupervisedLoss, args...;
-                     label = string(loss),
-                     kw...)
-    plot!(plt, value_fun(loss), args...;
-          label = label,
-          kw...)
-end
-
-function Plots.plot!{T<:SupervisedLoss}(plt::Plots.Plot, vec::Vector{T}, args...; kw...)
-    @assert length(vec) > 0
-    plt = plot!(vec[1], args...; kw...)
-    for loss in vec[2:end]
-        plot!(plt, loss, args...)
-    end
-    plt
+    map(value_fun, losses), args...
 end
