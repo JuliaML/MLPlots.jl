c34e305374380fb3b088ce3fc4f71c2de5ee5377
Thomas Breloff <tom@breloff.com>
2015-12-16 22:27:10 -0500

    cleanup ROC

--- src/ROCAnalysis/roc.jl
+++ src/ROCAnalysis/roc.jl
@@ -1,37 +1,17 @@
-using Plots
-using ROCAnalysis
+
+import ROCAnalysis
 
 function Plots._apply_recipe(d::Dict, roc::ROCAnalysis.Roc; kw...)
 
     get!(d, :legend, false)
-    get!(d, :xlims, (0,1))
-    get!(d, :ylims, (0,1))
     get!(d, :xlabel, "False Positive Rate")
     get!(d, :ylabel, "True Positive Rate")
+    get!(d, :linestyle, [:solid :dash])
+    get!(d, :linealpha, [1.0 0.5])
 
-    xargs = []
-    yargs = []
-    linealpha = Float64[]
-    linestyle = Symbol[]
+    auc_ann = @sprintf("AUC %1.3f", 1 - ROCAnalysis.auc(roc))
+    get!(d, :annotation, (1, 0, text(auc_ann, :right, :bottom, 12, :monospace)))
 
-    push!(xargs, roc.pfa) # probability of false alarm, false positive rate, 1 - specificity
-    # roc.pmiss (probability of miss, 1 - true positive rate, 1 - sensitivity)
-    push!(yargs, 1 .- roc.pmiss) # true positive rate, sensitivity
-    push!(linealpha, 1.0)
-    push!(linestyle, :solid)
-
-    push!(xargs, [0, 1])
-    push!(yargs, [0, 1])
-    push!(linealpha, 0.5)
-    push!(linestyle, :dash)
-
-    d[:linealpha] = reshape(linealpha, 1, length(linealpha))
-    d[:linestyle] = reshape(linestyle, 1, length(linestyle))
-
-    # if plt.n == 0
-    #    AUC = round(1 - ROCAnalysis.auc(roc), 4)
-    #    get!(d, :annotation, (0.9, 0.05, text("AUC $(AUC)", :center, 10, :monospace)))
-    # end
-
-    xargs, yargs
+    # xargs, yargs
+    Any[roc.pfa, 0:1], Any[1 .- roc.pmiss, 0:1]    
 end
