d46faf9e1b30b6ff14338c6506a40f22bbfb3a82
Thomas Breloff <tom@breloff.com>
2016-05-03 09:47:40 -0400

    travis

--- test/travis_commands.jl
+++ test/travis_commands.jl
@@ -5,4 +5,10 @@ Pkg.clone("https://github.com/Evizero/LearnBase.jl.git")
 Pkg.clone("https://github.com/tbreloff/OnlineAI.jl.git")
 Pkg.clone("https://github.com/tbreloff/QuickStructs.jl.git")
 
+ENV["PYTHON"] = ""
+Pkg.add("PyPlot")
+Pkg.build("PyPlot")
+
+Pkg.checkout("Plots")
+
 Pkg.test("MLPlots"; coverage=false)
