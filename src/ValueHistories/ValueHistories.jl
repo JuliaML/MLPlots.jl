65a62689a7fe91c0f4d8f45bdac5d0c2a773a69a
Thomas Breloff <tom@breloff.com>
2016-05-05 12:23:04 -0400

    working on switch to plotrecipe

--- src/ValueHistories/ValueHistories.jl
+++ src/ValueHistories/ValueHistories.jl
@@ -7,35 +7,42 @@ _is_plotable_history{I,V<:Real}(::VectorUnivalueHistory{I,V}) = true
 _filter_plotable_histories(h::DynMultivalueHistory) =
     filter((k,v) -> _is_plotable_history(v), h.storage)
 
-function Plots._apply_recipe{I,V<:Real}(d::KW, h::VectorUnivalueHistory{I,V}; kw...)
-    get!(d, :markershape, :ellipse)
-    get!(d, :markersize, 1.5)
-    get!(d, :legend, false)
+# function Plots._apply_recipe{I,V<:Real}(d::KW, h::VectorUnivalueHistory{I,V}; kw...)
+#     get!(d, :markershape, :ellipse)
+#     get!(d, :markersize, 1.5)
+#     get!(d, :legend, false)
+#     get(h)
+# end
+
+@plotrecipe h::Union{VectorUnivalueHistory,QueueUnivalueHistory} begin
+    :markershape => :ellipse
+    :markersize => 1.5
+    :legend => false
     get(h)
 end
 
-function Plots._apply_recipe{I,V<:Real}(d::KW, h::QueueUnivalueHistory{I,V}; kw...)
-    get!(d, :markershape, :ellipse)
-    get!(d, :markersize, 1.5)
-    get!(d, :legend, false)
-    get(h)
-end
+# function Plots._apply_recipe{I,V<:Real}(d::KW, h::QueueUnivalueHistory{I,V}; kw...)
+#     get!(d, :markershape, :ellipse)
+#     get!(d, :markersize, 1.5)
+#     get!(d, :legend, false)
+#     get(h)
+# end
 
-function Plots._apply_recipe(d::KW, h::DynMultivalueHistory; issubplot = false, kw...)
+
+@plotrecipe h::DynMultivalueHistory begin
     filtered = _filter_plotable_histories(h)
     k_vec = [k for (k, v) in filtered]
     v_vec = [v for (k, v) in filtered]
     if length(v_vec) > 0
-        get!(d, :markershape, :ellipse)
-        get!(d, :markersize, 1.5)
-        get!(d, :title, "Multivalue History")
-        get!(d, :legend, true)
-        get!(d, :label, map(string, k_vec)')
-        if issubplot
-            if get!(d, :n, length(v_vec)) == length(v_vec)
-                d[:legend] = false
-                d[:title] = d[:label]
-            end
+        :markershape => :ellipse
+        :markersize => 1.5
+        :label => map(string, k_vec)'
+        if issubplot && get!(d, :n, length(v_vec)) == length(v_vec)
+            :title => d[:label]
+            :legend => false
+        else
+            :title => "Multivalue History"
+            :legend => true
         end
         get_vec = map(get, v_vec)
         [x for (x, y) in get_vec], [y for (x, y) in get_vec]
@@ -43,3 +50,27 @@ function Plots._apply_recipe(d::KW, h::DynMultivalueHistory; issubplot = false, 
         throw(ArgumentError("Can't plot an empty history, nor a history with strange types"))
     end
 end
+
+#
+# function Plots._apply_recipe(d::KW, h::DynMultivalueHistory; issubplot = false, kw...)
+#     filtered = _filter_plotable_histories(h)
+#     k_vec = [k for (k, v) in filtered]
+#     v_vec = [v for (k, v) in filtered]
+#     if length(v_vec) > 0
+#         get!(d, :markershape, :ellipse)
+#         get!(d, :markersize, 1.5)
+#         get!(d, :title, "Multivalue History")
+#         get!(d, :legend, true)
+#         get!(d, :label, map(string, k_vec)')
+#         if issubplot
+#             if get!(d, :n, length(v_vec)) == length(v_vec)
+#                 d[:legend] = false
+#                 d[:title] = d[:label]
+#             end
+#         end
+#         get_vec = map(get, v_vec)
+#         [x for (x, y) in get_vec], [y for (x, y) in get_vec]
+#     else
+#         throw(ArgumentError("Can't plot an empty history, nor a history with strange types"))
+#     end
+# end
--- test/runtests.jl
+++ test/runtests.jl
@@ -43,66 +43,66 @@ end
 
 # ---------------------------------------------------------
 
-
-facts("LearnBase") do
-    with(size=(300,300), guidefont=font(9), tickfont=font(7)) do
-        @plottest "learnbase1" begin
-            plot(LossFunctions.HingeLoss())
-        end
-        @plottest "learnbase2" begin
-            plot([ZeroOneLoss(), L1HingeLoss(), L2HingeLoss(), LogitMarginLoss()])
-        end
-        @plottest "learnbase3" begin
-            subplot([ZeroOneLoss(), L1HingeLoss(), L2HingeLoss(), LogitMarginLoss()], size=(400,400))
-        end
-        @plottest "learnbase4" begin
-            plot(LossFunctions.L2DistLoss())
-        end
-        @plottest "learnbase5" begin
-            plot([L2DistLoss(), L1DistLoss(), EpsilonInsLoss(.4), LogitDistLoss()])
-        end
-        @plottest "learnbase6" begin
-            subplot([L2DistLoss(), L1DistLoss(), EpsilonInsLoss(.4), LogitDistLoss()], size=(400,400))
-        end
-    end
-end
-
-# ---------------------------------------------------------
-
-facts("CorrPlot") do
-    @plottest "corrplot" begin
-        M = randn(1000, 4)
-        M[:,2] += 0.8M[:,1]
-        M[:,3] -= 0.7M[:,1]
-        corrplot(M, size=(700,700))
-    end
-end
-
-# ---------------------------------------------------------
-
-facts("OnlineAI") do
-
-    # build and fit a neural net, then show it
-    @plottest "onlineai1" begin
-        net = buildClassificationNet(3,1,[15,10,5])
-        for i in 1:1000
-            x = rand(3)
-            fit!(net, x, Float64[sum(x)])
-        end
-        plot(net)
-    end
-
-    @plottest "onlineai2" begin
-        n = 20
-        spikes = SpikeTrains(n, title = "Spike Trains", color = :darkblue)
-        for t=1:100, i=1:n
-            if rand() < 0.1
-                push!(spikes, i, t)
-            end
-        end
-        spikes.plt
-    end
-end
+#
+# facts("LearnBase") do
+#     with(size=(300,300), guidefont=font(9), tickfont=font(7)) do
+#         @plottest "learnbase1" begin
+#             plot(LossFunctions.HingeLoss())
+#         end
+#         @plottest "learnbase2" begin
+#             plot([ZeroOneLoss(), L1HingeLoss(), L2HingeLoss(), LogitMarginLoss()])
+#         end
+#         @plottest "learnbase3" begin
+#             subplot([ZeroOneLoss(), L1HingeLoss(), L2HingeLoss(), LogitMarginLoss()], size=(400,400))
+#         end
+#         @plottest "learnbase4" begin
+#             plot(LossFunctions.L2DistLoss())
+#         end
+#         @plottest "learnbase5" begin
+#             plot([L2DistLoss(), L1DistLoss(), EpsilonInsLoss(.4), LogitDistLoss()])
+#         end
+#         @plottest "learnbase6" begin
+#             subplot([L2DistLoss(), L1DistLoss(), EpsilonInsLoss(.4), LogitDistLoss()], size=(400,400))
+#         end
+#     end
+# end
+#
+# # ---------------------------------------------------------
+#
+# facts("CorrPlot") do
+#     @plottest "corrplot" begin
+#         M = randn(1000, 4)
+#         M[:,2] += 0.8M[:,1]
+#         M[:,3] -= 0.7M[:,1]
+#         corrplot(M, size=(700,700))
+#     end
+# end
+#
+# # ---------------------------------------------------------
+#
+# facts("OnlineAI") do
+#
+#     # build and fit a neural net, then show it
+#     @plottest "onlineai1" begin
+#         net = buildClassificationNet(3,1,[15,10,5])
+#         for i in 1:1000
+#             x = rand(3)
+#             fit!(net, x, Float64[sum(x)])
+#         end
+#         plot(net)
+#     end
+#
+#     @plottest "onlineai2" begin
+#         n = 20
+#         spikes = SpikeTrains(n, title = "Spike Trains", color = :darkblue)
+#         for t=1:100, i=1:n
+#             if rand() < 0.1
+#                 push!(spikes, i, t)
+#             end
+#         end
+#         spikes.plt
+#     end
+# end
 
 
 # ---------------------------------------------------------\
