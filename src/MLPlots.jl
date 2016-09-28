8a8f0fb742ae1a93022430523aae0c34bf3596d0
Thomas Breloff <tom@breloff.com>
2016-09-28 15:39:08 -0400

    removed ValueHistories; reorganized; added TracePlot

--- src/MLPlots.jl
+++ src/MLPlots.jl
@@ -1,15 +1,39 @@
 module MLPlots
 
 using Reexport
-@reexport using Plots
-using RecipesBase
-using Requires
+@reexport using PlotRecipes
 
 export
-    corrplot
+    TracePlot
 
-include("recipes.jl")
-@require ValueHistories   include("ValueHistories/ValueHistories.jl")
+# ---------------------------------------------------------------------
+
+# a helper class to track many variables at once over time
+type TracePlot{I,T}
+    indices::I
+    plt::Plot{T}
+end
+function TracePlot(n::Int = 1; maxn::Int = typemax(Int), kw...)
+    indices = if n > maxn
+        # sample maxn
+        shuffle(1:n)[1:maxn]
+    else
+        1:n
+    end
+    plt = plot(length(indices); kw...)
+    TracePlot(indices, plt)
+end
+function add_data(tp::TracePlot, x::Number, y::AbstractVector)
+    for (i,idx) in enumerate(tp.indices)
+        push!(tp.plt.series_list[i], x, y[idx])
+    end
+end
+add_data(tp::TracePlot, x::Number, y::Number) = add_data(tp, x, [y])
+
+# ---------------------------------------------------------------------
+# optional
+
+using Requires
 @require OnlineAI         include("OnlineAI/onlineai.jl")
 @require ROCAnalysis      include("ROCAnalysis/roc.jl")
 
