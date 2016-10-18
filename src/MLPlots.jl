06eb8ef02358d7e87dea802b5b2331d7c321c61d
Thomas Breloff <tom@breloff.com>
2016-10-18 11:31:49 -0400

    ChainPlot

--- src/MLPlots.jl
+++ src/MLPlots.jl
@@ -2,6 +2,7 @@ module MLPlots
 
 using Reexport
 @reexport using PlotRecipes
+import Plots: Series
 using LearnBase
 
 export
@@ -13,20 +14,27 @@ export
 type TracePlot{I,T}
     indices::I
     plt::Plot{T}
+    sp::Subplot{T}
+    series::Vector{Series}
 end
-function TracePlot(n::Int = 1; maxn::Int = typemax(Int), kw...)
+function TracePlot(n::Int = 1; maxn::Int = typemax(Int), sp = nothing, kw...)
     indices = if n > maxn
         # sample maxn
         shuffle(1:n)[1:maxn]
     else
         1:n
     end
-    plt = plot(length(indices); kw...)
-    TracePlot(indices, plt)
+    if sp == nothing
+        plt = plot(length(indices); kw...)
+        sp = plt[1]
+    else
+        plt = plot!(sp, length(indices); kw...)
+    end
+    TracePlot(indices, plt, sp, sp.series_list)
 end
 function Base.push!(tp::TracePlot, x::Number, y::AbstractVector)
     for (i,idx) in enumerate(tp.indices)
-        push!(tp.plt.series_list[i], x, y[idx])
+        push!(tp.series[i], x, y[idx])
     end
 end
 Base.push!(tp::TracePlot, x::Number, y::Number) = push!(tp, x, [y])
@@ -49,9 +57,9 @@ end
 if is_installed("Transformations")
     include("optional/transformations.jl")
 end
-if is_installed("OnlineAI")
-    include("optional/onlineai.jl")
-end
+# if is_installed("OnlineAI")
+#     include("optional/onlineai.jl")
+# end
 if is_installed("ROCAnalysis")
     include("optional/roc.jl")
 end
--- src/optional/transformations.jl
+++ src/optional/transformations.jl
@@ -1,33 +1,101 @@
 
 using Transformations
 
+export
+    ChainPlot
+
+# -------------------------------------------------------------------------
+
+#=
+A ChainPlot is a bunch of TracePlots that are updated with the param/input/output
+values and gradients from a Transformations.Chain.  An example:
+
+using MLPlots, Transformations
+
+# build a neural net
+nn = nnet(2,3,[2,10],:softplus)
+
+# build the ChainPlot.  attributes pass through to the plot
+cp = ChainPlot(nn, size=(1000,1000))
+
+for i=1:20
+    # make some random updates to the Chain
+    transform!(nn, rand(2))
+    grad!(nn, rand(3))
+
+    # update the ChainPlot and display it
+    update!(cp)
+    gui()
+=#
+
 # the maps map chain transformation index to subplot index
 type ChainPlot
     chain::Chain
     plt::Plot
-    subplots::Vector{Subplot}
-    θmap::Dict
-    ∇map::Dict
-    xmap::Dict
-    ymap::Dict
+    nt::Int # number of transformations
+    np::Int # number of params
+    lastx::Float64
+    θtps::Vector{TracePlot}
+    ∇tps::Vector{TracePlot}
+    ytps::Vector{TracePlot}
+    dytps::Vector{TracePlot}
 end
 
-function ChainPlot(chain::Chain)
+function add_traceplots(plt, valtps, gradtps, n, idxbase, i, idxdiff, title, add_yguide, kw)
+    push!(valtps, TracePlot(n; sp=plt[idxbase+i], title=title, yguide=(add_yguide ? "Value" : ""), kw...))
+    push!(gradtps, TracePlot(n; sp=plt[idxbase+i+idxdiff], yguide=(add_yguide ? "Grad" : ""), kw...))
 end
 
-# parameter plots
-pidx = 1:2:length(t)
-pvalplts = [TracePlot(length(params(t[i])), title="$(t[i])") for i=pidx]
-ylabel!(pvalplts[1].plt, "Param Vals")
-pgradplts = [TracePlot(length(params(t[i]))) for i=pidx]
-ylabel!(pgradplts[1].plt, "Param Grads")
+function ChainPlot(chain::Chain; kw...)
+    nparams = count(t->length(params(t))>0, chain.ts)
+    n = length(chain.ts)
 
-# nnet plots of values and gradients
-valinplts = [TracePlot(input_length(t[i]), title="input", yguide="Layer Value") for i=1:1]
-valoutplts = [TracePlot(output_length(t[i]), title="$(t[i])", titlepos=:left) for i=1:length(t)]
-gradinplts = [TracePlot(input_length(t[i]), yguide="Layer Grad") for i=1:1]
-gradoutplts = [TracePlot(output_length(t[i])) for i=1:length(t)]
+    plt = plot(layout = @layout([grid(2,nparams)
+                                 grid(2,n+1)]),
+                leg=false, titlefont=font(10))
 
-# loss/accuracy plots
-lossplt = TracePlot(title="Test Loss", ylim=(0,Inf))
-accuracyplt = TracePlot(title="Accuracy", ylim=(0.6,1))
+    θtps = TracePlot[]
+    ∇tps = TracePlot[]
+    ytps = TracePlot[]
+    dytps = TracePlot[]
+    pidx = 1
+    for (i,t) in enumerate(chain.ts)
+        np = length(params(t))
+        if np > 0
+            add_traceplots(plt, θtps, ∇tps, np, 0, pidx, nparams, string(t), i==1, kw)
+            pidx += 1
+        end
+
+        if i == 1
+            nin = input_length(t)
+            add_traceplots(plt, ytps, dytps, nin, 2nparams, i, n, "Input", true, kw)
+        end
+
+        nout = output_length(t)
+        add_traceplots(plt, ytps, dytps, nout, 2nparams+1, i, n, string(t), false, kw)
+    end
+
+    ChainPlot(chain, plt, n, nparams, 0.0, θtps, ∇tps, ytps, dytps)
+end
+
+function LearnBase.update!(cp::ChainPlot, x = cp.lastx+1)
+    pidx = 1
+    for (i,t) in enumerate(cp.chain.ts)
+        np = length(params(t))
+
+        if np > 0
+            push!(cp.θtps[pidx], x, params(t))
+            push!(cp.∇tps[pidx], x, grad(t))
+            pidx += 1
+        end
+        idxbase = 2 * cp.np + 1
+        if i == 1
+            push!(cp.ytps[1], x, input_value(t))
+            push!(cp.dytps[1], x, input_grad(t))
+        end
+        push!(cp.ytps[i+1], x, output_value(t))
+        push!(cp.dytps[i+1], x, output_grad(t))
+    end
+    cp.lastx = x
+    return
+end
